ef2fc803ebc5559d9c0f7c69892dc338b3bf5f79
brandon <brandonlastomo1@gmail.com>
2023-11-20 19:56:15 +0700

    handle size input <= 0 and additional update
    Additional update:
    - print different sentence when the size of array is 1.

--- pertemuan6/tugasP6.cpp
+++ pertemuan6/tugasP6.cpp
@@ -5,24 +5,41 @@ int main(){
     cout << "Selamat Datang di Program Pencari Nilai Tertinggi\n\n";
 
     int sizeArr; // variabel wadah ukuran array
-    cout << "berapa banyak nilai yang akan Anda periksa?\n> ";
-    // input user untuk ukuran array
-    cin >> sizeArr;
-    // deklarasi array dengan ukuran array berdasarkan input user
-    int nilai[sizeArr];
+
+    // handle input user <= 0
+    do{
+        cout << "Berapa banyak nilai yang akan Anda periksa?\n> ";
+        // input user untuk ukuran array
+        cin >> sizeArr;
+        if (sizeArr <= 0){
+            cout << "Banyak nilai harus lebih dari 0. Silakan coba lagi.\n\n";
+        }
+    } while (sizeArr <= 0);
+    // akhir dari handle
+    
+    int nilai[sizeArr]; // deklarasi array dengan ukuran array berdasarkan input user
     
     cout << endl;
 
     // loop untuk input value array
     for (int a = 0; a < sizeArr; a++){
-        cout << "masukkan nilai ke-" << a+1 << ": ";
+        cout << "Masukkan nilai ke-" << a+1 << ": ";
         // input value ke array nilai
         cin >> nilai[a];
     }
+    // akhir dari loop input value array
     
     
-    cout << "\nnilai tertinggi dari nilai-nilai yang Anda masukkan adalah: ";
+    if (sizeArr == 1){
+        // kalimat jika hanya ada satu data
+        cout << "\nKarena hanya ada satu nilai, maka nilai tertingginya adalah: ";
+    } else {
+        // jika lebih dari satu data
+        cout << "\nNilai tertinggi dari nilai-nilai yang Anda masukkan adalah: ";
+    }
     
+    
+    // loop untuk mencari nilai tertinggi
     for (int b = 0; b < sizeArr; b++){
         // jika indeks+1 sama dengan ukuran array, keluarkan nilai array terakhir
         if (b+1 == sizeArr){
@@ -38,6 +55,7 @@ int main(){
             }
         }
     }
+    // akhir dari loop untuk mencari nilai tertinggi
     
     return 0;
 }
